a0a8ebfcacc9b49afe23b731bab0005b259b76ce
Shirow1302 <belickov.vadick2017@yandex.ru>
2023-10-22 15:22:12 +0300

    added capitalize_first_letters function with docstring

--- str_func.py
+++ str_func.py
@@ -1,2 +1,16 @@
 def to_uppercase(input_string):
     return input_string.upper()
+
+def capitalize_first_letters(input_string):
+    """
+    Capitalizes the first letter of each word in the input string.
+
+    Args:
+        input_string (str): The input string.
+
+    Returns:
+        str: The input string with the first letter of each word capitalized.
+    """
+    words = input_string.split()
+    capitalized_words = [word.capitalize() for word in words]
+    return " ".join(capitalized_words)
